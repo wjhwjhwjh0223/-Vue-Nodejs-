fbdcff7f6685244a267e9c5a423322a9f3340150
wujunhao <junjun0223@outlook.com>
2023-12-23 23:42:55 +0800

    modified:   src/views/fuwuguanli/fuwuzhuangtai.vue 	modified:   src/views/jinjishijian/shijianliebiao.vue

--- src/views/fuwuguanli/fuwuzhuangtai.vue
+++ src/views/fuwuguanli/fuwuzhuangtai.vue
@@ -4,16 +4,107 @@
             <div slot="header">
                 <span>服务状态</span>
             </div>
-    </el-card>
+        </el-card>
+        <div class="service-status-container">
+            <!-- 顶部功能区域，包含搜索和筛选 -->
+            <el-row :gutter="20" class="search-filter-row">
+                <el-col :span="8">
+                    <el-input v-model="search" placeholder="请输入服务编号搜索..." @keyup.enter="fetchServices" clearable>
+                        <!-- 在el-input内部使用slot插槽添加搜索按钮 -->
+                        <el-button slot="append" icon="el-icon-search" @click="fetchServices">
+                            搜索
+                        </el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="16">
+                    <el-select v-model="filterStatus" placeholder="选择状态" @change="fetchServices">
+                        <el-option label="全部" value=""></el-option>
+                        <el-option label="完成" value="completed"></el-option>
+                        <el-option label="进行中" value="in_progress"></el-option>
+                        <el-option label="等待接取" value="pending"></el-option>
+                    </el-select>
+                </el-col>
+            </el-row>
+            <!-- 服务列表表格 -->
+            <el-table :data="filteredServices" style="width: 100%" stripe>
+                <el-table-column prop="id" label="服务编号" width="100"></el-table-column>
+                <el-table-column prop="name" label="服务名称" width="180"></el-table-column>
+                <el-table-column prop="status" label="状态" width="180">
+                    <template v-slot="scope">
+                        <el-tag :type="statusTagType(scope.row.status)">
+                            {{ statusText(scope.row.status) }}
+                        </el-tag>
+                    </template>
+                </el-table-column>
+                <!-- 其他列 -->
+            </el-table>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    data() {
+        return {
+            search: '', // 搜索关键词
+            filterStatus: '', // 筛选的状态
+            services: [], // 服务列表数据
+        };
+    },
+    computed: {
+        // 根据搜索关键词和筛选状态计算过滤后的服务列表
+        filteredServices() {
+            return this.services.filter(service => {
+                return (
+                    (this.filterStatus ? service.status === this.filterStatus : true) &&
+                    (this.search ? service.name.includes(this.search) : true)
+                );
+            });
+        },
+    },
+    methods: {
+        fetchServices() {
+            // 从服务器获取服务列表，根据search和filterStatus筛选
+        },
+        statusTagType(status) {
+            // 根据状态返回不同的标签类型
+            switch (status) {
+                case 'completed':
+                    return 'success';
+                case 'in_progress':
+                    return 'info';
+                case 'pending':
+                    return 'warning';
+                default:
+                    return 'default';
+            }
+        },
+        statusText(status) {
+            // 根据状态返回文本描述
+            switch (status) {
+                case 'completed':
+                    return '完成';
+                case 'in_progress':
+                    return '进行中';
+                case 'pending':
+                    return '等待接取';
+                default:
+                    return '未知';
+            }
+        },
+    },
+    created() {
+        this.fetchServices();
+    },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.service-status-container {
+    padding: 20px;
+}
 
+.search-filter-row {
+    margin-bottom: 20px;
+}
 </style>
--- src/views/jinjishijian/shijianliebiao.vue
+++ src/views/jinjishijian/shijianliebiao.vue
@@ -1,19 +1,110 @@
 <template>
     <div>
         <el-card>
-        <div slot="header">
-            <span>紧急事件列表</span>
+            <div slot="header">
+                <span>紧急事件列表</span>
+            </div>
+        </el-card>
+        <div class="emergency-events">
+            <el-row>
+                <el-col :span="24">
+                    <el-card class="card-container">
+                        <div slot="header" class="header">
+                            紧急事件列表
+                        </div>
+                        <el-row :gutter="20">
+                            <el-col :span="24">
+                                <el-input v-model="searchQuery" placeholder="搜索事件编号或标题..." clearable
+                                    @keyup.enter="fetchEvents" class="search-input">
+                                    <el-button slot="append" icon="el-icon-search" @click="fetchEvents">搜索</el-button>
+                                </el-input>
+                            </el-col>
+                        </el-row>
+                        <el-row :gutter="20">
+                            <el-col :span="24">
+                                <el-select v-model="selectedStatus" placeholder="选择状态" class="status-select"
+                                    @change="fetchEvents">
+                                    <el-option label="所有" value=""></el-option>
+                                    <el-option label="未处理" value="unhandled"></el-option>
+                                    <el-option label="处理中" value="in_progress"></el-option>
+                                    <el-option label="已解决" value="resolved"></el-option>
+                                </el-select>
+                            </el-col>
+                        </el-row>
+                        <el-table :data="events" stripe class="events-table">
+                            <!-- 表格列 -->
+                        </el-table>
+                    </el-card>
+                </el-col>
+            </el-row>
         </div>
-    </el-card>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    data() {
+        return {
+            searchQuery: '',
+            selectedStatus: '',
+            events: [], // 事件数据
+        };
+    },
+    methods: {
+        fetchEvents() {
+            // 调用API获取事件列表，根据searchQuery和selectedStatus进行筛选
+        },
+        statusType(status) {
+            // 根据事件状态返回对应的标签类型
+            switch (status) {
+                case 'unhandled': return 'danger';
+                case 'in_progress': return 'warning';
+                case 'resolved': return 'success';
+                default: return 'info';
+            }
+        },
+        handleEvent(event) {
+            // 处理事件的逻辑
+            console.log('Handling event:', event);
+        },
+    },
+    mounted() {
+        this.fetchEvents();
+    },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.emergency-events {
+    padding: 20px;
+}
 
+.card-container {
+    border: 1px solid #ebeef5;
+    border-radius: 4px;
+}
+
+.header {
+    background-color: #f5f7fa;
+    line-height: 36px;
+    font-size: 15px;
+    color: #303133;
+}
+
+.search-input {
+    width: 100%;
+    margin-bottom: 20px;
+    /* 与下拉选择间距 */
+}
+
+.status-select {
+    width: 100%;
+    margin-bottom: 20px;
+    /* 与表格间距 */
+}
+
+.events-table {
+    width: 100%;
+    /* 表格宽度 */
+}
 </style>
