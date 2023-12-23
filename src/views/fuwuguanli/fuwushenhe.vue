4a048ca784f3f4e9733dcd0685e85efd9fbe08f4
wujunhao <junjun0223@outlook.com>
2023-12-23 22:30:25 +0800

    modified:   src/views/fuwuguanli/fuwushenhe.vue 	modified:   src/views/user/gerenzhongxin.vue

--- src/views/fuwuguanli/fuwushenhe.vue
+++ src/views/fuwuguanli/fuwushenhe.vue
@@ -1,19 +1,107 @@
 <template>
     <div>
-        <el-card>
-            <div slot="header">
-                <span>服务审核</span>
-            </div>
-    </el-card>
+        <div class="audit-container">
+            <el-card class="audit-card">
+                <div slot="header" class="clearfix">
+                    <span>服务审核</span>
+                </div>
+            </el-card>
+            <el-container>
+                <el-header>
+                    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+                        <el-menu-item index="1">首页</el-menu-item>
+                        <el-menu-item index="2">服务列表</el-menu-item>
+                        <el-menu-item index="3">审核服务</el-menu-item>
+                        <el-menu-item index="4">用户管理</el-menu-item>
+                    </el-menu>
+                </el-header>
+                <el-main>
+                    <div class="search-area">
+                        <el-input v-model="search" placeholder="通过编号搜索服务..." class="search-input"></el-input>
+                        <el-button type="primary" icon="el-icon-search" @click="searchService">搜索</el-button>
+                    </div>
+                    <el-table :data="filteredServiceList" style="width: 100%" stripe>
+                        <el-table-column prop="id" label="服务编号" width="100"></el-table-column>
+                        <el-table-column prop="date" label="申请日期" width="180"></el-table-column>
+                        <el-table-column prop="name" label="申请者" width="200"></el-table-column>
+                        <el-table-column prop="type" label="服务类型" width="200"></el-table-column>
+                        <el-table-column label="操作" width="180">
+                            <template slot-scope="scope">
+                                <el-button type="primary" size="small"
+                                    @click="approveService(scope.$index, scope.row)">批准</el-button>
+                                <el-button type="danger" size="small"
+                                    @click="rejectService(scope.$index, scope.row)">拒绝</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-main>
+            </el-container>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data() {
+        return {
+            activeIndex: '3', // 当前活动菜单的索引
+            search: '', // 搜索框数据绑定
+            serviceList: [
+                { id: '001', date: '2023-12-22', name: '张三', type: '家政服务' },
+                { id: '002', date: '2023-12-20', name: '李四', type: '健康咨询' },
+                // 更多服务数据...
+            ],
+        };
+    },
+    computed: {
+        filteredServiceList() {
+            if (!this.search) {
+                return this.serviceList;
+            }
+            return this.serviceList.filter(service => service.id.includes(this.search));
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleRowClick(row, column, event) {
+            // 可以在这里处理行点击事件
+        },
+        searchService() {
+            // 这里可以实现更复杂的搜索逻辑，如果需要的话
+            // 由于我们使用了计算属性，所以这里实际上不需要执行任何操作，搜索逻辑已经在计算属性中定义
+        },
+        approveService(index, row) {
+            console.log('批准服务:', row);
+            // 在这里调用API来批准服务
+        },
+        rejectService(index, row) {
+            console.log('拒绝服务:', row);
+            // 在这里调用API来拒绝服务
+        }
     }
+};
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
+.audit-container {
+    margin: 20px;
+}
 
+.audit-card {
+    margin-bottom: 20px;
+}
+
+.search-area {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+.search-input {
+    margin-right: 10px;
+    flex: 1; /* Allows input to fill space */
+}
 </style>
--- src/views/user/gerenzhongxin.vue
+++ src/views/user/gerenzhongxin.vue
@@ -1,15 +1,77 @@
 <template>
-    <el-card>
-        <div slot="header">
-            <span>个人中心</span>
-        </div>
-    </el-card>
+    <el-container>
+        <el-aside width="300px" style="background-color: #f2f2f5; padding: 20px;">
+            <!-- 头像和姓名 -->
+            <el-image style="border-radius: 50%; width: 100px; height: 100px; margin: 0 auto;"
+                src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsafe-img.xhscdn.com%2Fbw1%2Fabf9e746-a09c-4b08-bcba-1e347a370226%3FimageView2%2F2%2Fw%2F1080%2Fformat%2Fjpg&refer=http%3A%2F%2Fsafe-img.xhscdn.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1705931166&t=e14ea8b2e19ef3bfebf000a493a92dca"
+                fit="cover"></el-image>
+            <p style="text-align: center; margin-top: 20px;">吴骏浩</p>
+
+            <!-- 个人信息描述列表 -->
+            <el-descriptions column="1" style="margin-top: 20px;">
+                <el-descriptions-item label="职业">前端开发</el-descriptions-item>
+                <el-descriptions-item label="公司">xxx</el-descriptions-item>
+                <el-descriptions-item label="位置">重庆</el-descriptions-item>
+                <el-descriptions-item label="特别">男</el-descriptions-item>
+                <el-descriptions-item label="毕业院校">重庆科技大学</el-descriptions-item>
+                <el-descriptions-item label="邮箱">junjun02223@outlook.com</el-descriptions-item>
+                <el-descriptions-item label="联系方式">wx:junjun010223</el-descriptions-item>
+                <el-descriptions-item label="技术栈">JavaScript, HTML, CSS, Vue, Node, React</el-descriptions-item>
+            </el-descriptions>
+
+            <!-- 编辑资料按钮 -->
+            <div style="text-align: center; margin-top: 20px;">
+                <el-button type="primary">编辑资料</el-button>
+            </div>
+        </el-aside>
+
+        <el-main>
+            <!-- 个性签名 -->
+            <div class="profile-section">
+                <p>原神!启动!</p>
+            </div>
+            <!-- 最喜欢的一句 -->
+            <div class="profile-section">
+
+                <blockquote>开心每一天</blockquote>
+            </div>
+
+            <!-- 座右铭或格言 -->
+            <div class="profile-section">
+                <p>只有经历过地狱的磨炼，才能炼就创造天堂的力量；只有流过血的手指，才能弹出世间的绝唱。</p>
+            </div>
+            <el-row>
+                <el-button type="primary" icon="el-icon-thumb" circle @click="handleUpvote"></el-button>
+                <el-button type="success" icon="el-icon-star-on" circle @click="handleFavorite"></el-button>
+                <el-button type="info" icon="el-icon-share" circle @click="handleShare"></el-button>
+            </el-row>
+
+        </el-main>
+    </el-container>
 </template>
+  
 
 <script>
 export default {
 
-}
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.profile-section {
+    border-bottom: 1px solid #ebeef5;
+    padding-bottom: 20px;
+    margin-bottom: 20px;
+}
+
+.el-aside {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.el-main {
+    padding: 0 20px;
+}
+</style>
+  
